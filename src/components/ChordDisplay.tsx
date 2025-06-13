0dc40f42b9a02333fa07e047ae2559dd47e0779e
tarakichi <tarakichi48@gmail.com>
2025-06-14 00:36:28 +0900

    add pianokey interface

--- src/components/ChordDisplay.tsx
+++ src/components/ChordDisplay.tsx
@@ -22,26 +22,27 @@ export default function ChordDisplay({ midiNotes }: Props) {
                     <p className='mx-1 my-3 py-1 px-2 text-zinc-50'>コードが見つかりません</p>
                 ) : (
                     <>
-                        <ul className='flex justify-center flex-wrap'>
+                        <div className='flex justify-center flex-wrap'>
                             {topChords.map((chord) => (
                                 <Tooltip.Provider delayDuration={100}>
                                     <Tooltip.Root>
                                         <Tooltip.Trigger asChild>
-                                            <li
+                                            <div
                                                 className="
                                                     mx-1 my-3 rounded-md font-medium text-zinc-50 bg-zinc-700 py-1 px-2
                                                     hover:shadow-lg hover:shadow-zinc-500/50 hover:brightness-110
                                                     transiton duration-200 ease-in-out
                                                 "
+                                                key={chord.fullName}
                                             >
                                                 {chord.fullName}
-                                            </li>
+                                            </div>
                                         </Tooltip.Trigger>
                                         <Tooltip.Portal>
                                             <Tooltip.Content
                                                 side="top"
                                                 align="center"
-                                                sideOffset={5}
+                                                sideOffset={3}
                                                 className="px-2 py-1 rounded bg-zinc-900 text-zinc-50 text-xs"
                                             >
                                                 一致度：{chord.score}%
@@ -51,21 +52,22 @@ export default function ChordDisplay({ midiNotes }: Props) {
                                     </Tooltip.Root>
                                 </Tooltip.Provider>
                             ))}
-                        </ul>
-                        <ul className='flex justify-center flex-wrap'>
+                        </div>
+                        <div className='flex justify-center flex-wrap'>
                             {otherChords.map((chord) => (
                                 <Tooltip.Provider delayDuration={100}>
                                     <Tooltip.Root>
                                         <Tooltip.Trigger asChild>
-                                            <li
+                                            <div
                                                 className="
                                                     mx-1 my-3 rounded-md font-medium text-zinc-50 bg-zinc-800 py-1 px-2
                                                     hover:text-shadow-lg hover:text-shadow-zinc-500/50
                                                     transiton duration-200 ease-in-out
                                                 "
+                                                key={chord.fullName}
                                             >
                                                 {chord.fullName}
-                                            </li>
+                                            </div>
                                         </Tooltip.Trigger>
                                         <Tooltip.Portal>
                                             <Tooltip.Content
@@ -81,7 +83,7 @@ export default function ChordDisplay({ midiNotes }: Props) {
                                     </Tooltip.Root>
                                 </Tooltip.Provider>
                             ))}
-                        </ul>
+                        </div>
                     </>
                 )}
             </div>
--- src/components/Keyboard.tsx
+++ src/components/Keyboard.tsx
@@ -12,57 +12,79 @@ function getNoteName(noteNumber: number) {
   return `${noteNames[note]}${octave}`;
 }
 
-const keys = [
-    { note: "C", isBlack: false },
-    { note: "C#", isBlack: true, blackKeyPosition: "left" },
-    { note: "D", isBlack: false },
-    { note: "D#", isBlack: true, blackKeyPosition: "right" },
-    { note: "E", isBlack: false },
-    { note: "F", isBlack: false },
-    { note: "F#", isBlack: true, blackKeyPosition: "left" },
-    { note: "G", isBlack: false },
-    { note: "G#", isBlack: true, blackKeyPosition: "center" },
-    { note: "A", isBlack: false },
-    { note: "A#", isBlack: true, blackKeyPosition: "right" },
-    { note: "B", isBlack: false },
-];
+interface PianoKey {
+    pitch: string;
+    noteName: string;
+    midi: number;
+    isBlack: boolean;
+    isKey: boolean;
+    blackKeyPosition?: "left" | "center" | "right";
+}
+
+
+function generateKeyboardRange(startNote: number, endNote: number) {
+    const result: PianoKey[] = [];
+
+    for (let midi = startNote; midi <= endNote; midi++) {
+        const note = noteNames[midi % 12];
+        const isBlack = note.includes("#");
+
+        result.push({
+            pitch: `${note}${Math.floor(midi / 12) - 1}`,
+            noteName: note,
+            midi,
+            isBlack,
+            isKey: true,
+            blackKeyPosition: isBlack
+                ? (note === "C#" || note === "F#") ? "left" :
+                    (note === "G#") ? "center" : "right"
+                : undefined,
+        });
+    }
+
+    return result;
+}
 
 export default function Keyboard({ activeNotes = [] }: Props) {
 
     const [isFullRange, setIsFullRange] = useState(false);
-    const uniqueActiveNotes = Array.from(new Set(activeNotes.map(n => getNoteName(n % 12).slice(0, -2))));
-
-    console.log(uniqueActiveNotes);
+    const activeNotesName = Array.from(activeNotes.map(n => getNoteName(n)));
+    const uniqueActiveNotesName = Array.from(new Set(activeNotesName.map(n => n.slice(0, -1))));
+    const keys = generateKeyboardRange(60, 72)
 
     return (
         <div className="w-screen flex flex-col justify-center items-center">
-            <div className="keyboard-toggle-container">
-                <label className="switch">
-                    <input
-                        type="checkbox"
-                        checked={isFullRange}
-                        onChange={() => setIsFullRange(!isFullRange)}
-                    />
-                    <span className="slider round"></span>
-                </label>
-                <span className="m-2 text-zinc-50">{isFullRange ? "88鍵" : "1オクターブ"}</span>
+            <div className="flex items-start w-4/5">
+                <div className="flex items-center mx-1">
+                    <label className="switch">
+                        <input
+                            id="keyboard-view-range"
+                            type="checkbox"
+                            checked={isFullRange}
+                            onChange={() => setIsFullRange(!isFullRange)}
+                        />
+                        <span className="slider round"></span>
+                    </label>
+                    <span className="m-2 text-zinc-50">{isFullRange ? "88鍵" : "1オクターブ"}</span>
+                </div>
             </div>
             <div className="flex justify-center mx-auto mt-2 mb-5 w-4/5 h-80">
                     <ul className="absolute white-keys flex">
                         {keys.filter(k => !k.isBlack).map(k => (
-                            <li key={k.note} className={`white-key ${uniqueActiveNotes.includes(k.note) ? "active" : ""}`}></li>
+                            <li key={k.pitch} className={`white-key ${uniqueActiveNotesName.includes(k.noteName) ? "active" : ""}`}></li>
                         ))}
                     </ul>
-                    <ul className="absolute black-keys flex z-10 translate-x-10">
+                    <ul className={`absolute black-keys flex z-10 ${keys[0].isBlack ? ("-translate-x-11") : ("-translate-x-4")}`}>
+                        {keys[0].isBlack ? <li className="black-key-frame"></li> : ""}
                         {keys.filter(k => k.isBlack).map(k => ( k.blackKeyPosition === "left" ? (
-                                <li key={k.note} className={`black-key-frame black-key ${uniqueActiveNotes.includes(k.note) ? "active" : ""}`}></li>
+                                <>
+                                    <li key={"before-" + k.pitch} className="black-key-frame"></li>
+                                    <li key={k.pitch} className={`black-key-frame black-key ${uniqueActiveNotesName.includes(k.noteName) ? "active" : ""}`}></li>
+                                </>
                             ) : ( k.blackKeyPosition === "center" ? (
-                                    <li key={k.note} className={`black-key-frame black-key ${uniqueActiveNotes.includes(k.note) ? "active" : ""}`}></li>
+                                    <li key={k.pitch} className={`black-key-frame black-key ${uniqueActiveNotesName.includes(k.noteName) ? "active" : ""}`}></li>
                                 ) : (
-                                    <>
-                                        <li key={k.note} className={`black-key-frame black-key ${uniqueActiveNotes.includes(k.note) ? "active" : ""}`}></li>
-                                        <li className="black-key-frame"></li>
-                                    </>
+                                    <li key={k.pitch} className={`black-key-frame black-key ${uniqueActiveNotesName.includes(k.noteName) ? "active" : ""}`}></li>
                                 )
                             )
                         ))}
