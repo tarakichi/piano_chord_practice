904732febee64db96bf1a1b775415e0f56b37593
nojiii <s1f102402899@iniad.org>
2025-06-18 10:57:41 +0900

    page arrangement

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import HomeMenu from "./pages/HomeMenu";
-import ChordViewer from "./pages/ChordViewer";
 import ChordPractice from "./pages/ChordPractice";
 
 function App() {
   return (
     <Router>
       <Routes>
-          <Route path="/" element={<HomeMenu/>} />
-          <Route path="/viewer" element={<ChordViewer/>} />
-          <Route path="/practice" element={<ChordPractice/>} />
+          <Route path="/" element={<ChordPractice/>} />
       </Routes>
     </Router>
   )
