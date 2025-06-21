6ff543501616448b30140d2e1b7ddd0027b1360b
tarakichi <tarakichi48@gmail.com>
2025-06-21 20:41:48 +0900

    layout change

--- src/components/TargetChordDisplay.tsx
+++ src/components/TargetChordDisplay.tsx
@@ -7,7 +7,7 @@ export default function TargetChordDisplay({ targetChord, isCorrect }: Props) {
     return (
         <div className="mr-5">
             <h2 className='text-zinc-50 font-bold text-xl'>目標コード</h2>
-            <div className="flex justify-center items-center bg-zinc-800 rounded-md my-2 mx-auto overflow-x-hidden overflow-y-auto max-w-4/5 h-28 w-100">
+            <div className="flex justify-center items-center bg-zinc-800 rounded-md my-2 mx-auto overflow-x-hidden overflow-y-auto h-28 w-100">
                 <div
                     className={`
                         mx-1 my-3 rounded-md font-medium text-4xl text-zinc-50 bg-zinc-700 py-5 p-10
