2005d79ce6e88ec43bfc1d46c2a3c06bef1e4da7
tarakichi <tarakichi48@gmail.com>
2025-08-26 21:17:19 +0900

    deploy settings

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/piano_chord_practice/",
   plugins: [
     react(),
     tailwindcss(),
